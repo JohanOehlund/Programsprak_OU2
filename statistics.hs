6ba8828a014399156b7951a300d9c92927c5a09c
c15jodHome <joah0087@student.umu.se>
2018-02-04 21:38:28 +0100

    ...

--- statistics.hs
+++ statistics.hs
@@ -2,6 +2,9 @@ import Data.List
 
 data Htree = Leaf Char | Branch Htree Htree deriving(Show)
 data Wtree = L Integer Char | B Integer Wtree Wtree deriving(Show)
+{-
+Deluppgift 1:
+-}
 
 {-
 Function: statistics
@@ -14,6 +17,15 @@ statistics (x:xs) = countLetters (x:xs) : statistics [res| res<-xs, res/=x]
 countLetters :: String -> (Integer,Char)
 countLetters (x:xs) = (sum [1|c<-(x:xs),c==x],x)
 
+{-
+Deluppgift 2
+-}
+maketree :: [(Integer, Char)] -> Htree
+maketree x = createHtree((createWtree)$(sortWtree)$(makeWtree x))  
+
+createHtree :: Wtree -> Htree
+createHtree (L i1 w1)  = (Leaf w1)
+createHtree (B i1 w1 w2) = (Branch ((createHtree) w1) ((createHtree) w2))
 
 makeWtree :: [(Integer, Char)] -> [Wtree]
 makeWtree ((i,c):[]) = [L i c]
@@ -21,35 +33,49 @@ makeWtree ((i,c):xs) = L i c : makeWtree xs
 
 sortWtree :: [Wtree] -> [Wtree]
 sortWtree [] = []
-sortWtree (x:xs) = sortBy (less') (x:xs)
---(sortBy) less' (x:xs)
+sortWtree (x:xs) = sortBy (increasing') (x:xs)
 
-less' :: Wtree -> Wtree-> Ordering
-less' (L x _) (L y _) = compare x y
+increasing' :: Wtree -> Wtree-> Ordering
+increasing' (L x _) (L y _)    = compare x y
+increasing' (B x _ _) (L y _)  = compare x y
+increasing' (L y _) (B x _ _)  = compare x y
+increasing' (B x _ _) (B y _ _)= compare x y
 
 
-createWtree :: [Wtree] -> [Wtree]  
+createWtree :: [Wtree] -> Wtree  
 createWtree x
-    | length x == 1 = x
-    | otherwise = createWtree (addWtree x)
+    | length x == 1 = head x
+    | otherwise = createWtree $(addWtree x)
 
 
 addWtree :: [Wtree] -> [Wtree]
-addWtree ((L i1 c1):(L i2 c2):xs) = (B (i1+i2) (L i1 c1) (L i2 c2):xs) 
-addWtree ((B i0 a b):(L i3 c3):xs)= (B (i0+i3) (B i0 a b) (L i3 c3):xs)
-addWtree ((L i3 c3):(B i0 a b):xs)= (B (i0+i3) (L i3 c3) (B i0 a b):xs)
-addWtree ((B i0 a b):(B i3 a2 b2):xs)= (B (i0+i3) (B i0 a b) (B i3 a2 b2):xs)
+addWtree ((L i1 c1):(L i2 c2):xs) = sortBy (increasing') 
+            (B (i1+i2) (L i1 c1) (L i2 c2):xs) 
+addWtree ((B i0 a b):(L i3 c3):xs)= sortBy (increasing') 
+            (B (i0+i3) (B i0 a b) (L i3 c3):xs)
+addWtree ((L i3 c3):(B i0 a b):xs)= sortBy (increasing') 
+            (B (i0+i3) (L i3 c3) (B i0 a b):xs)
+addWtree ((B i0 a b):(B i3 a2 b2):xs)= sortBy (increasing') 
+            (B (i0+i3) (B i0 a b) (B i3 a2 b2):xs)
 
-{-
-convertAndQuicksort [] = []  
-convertAndQuicksort ((reg,(hour,minute),(totHour,totMinute)):xs) =
-    (convertAndQuicksort [(a,b,time)|(a,b,time)<-xs, time>(totHour,totMinute)]) 
-                        ++[(reg,(totHour,totMinute))]++ 
-    (convertAndQuicksort [(a,b,time)|(a,b,time)<-xs, time<=(totHour,totMinute)])
--}
+
+
+encode :: String -> (Htree , [Integer])
+encode [] = []
+encode (x:xs) =(maketree(statistics (x:xs)),[1..10]) 
+
+traverseDF' :: Htree -> Char-> [Integer] -> [Integer]
+traverseDF' (Leaf c1) c output  = if c1==c then output else []
+traverseDF' (Branch l r) c output = (traverseDF' l c (0:output)) ++ (traverseDF' r c (1:output))
 {-
 let x = statistics "hejsan"
 let y = makeWtree x
 let z = sortWtree y
-createWtree z
+let w = createWtree z
+createHtree w
+
+2:
+let x = statistics "text"
+let y =maketree x
+traverseDF' y
 -}
