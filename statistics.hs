06ab97e015a9da31b42c335aaf378a89a9a0a640
c15jodHome <joah0087@student.umu.se>
2018-02-05 21:26:29 +0100

    hg

--- statistics.hs
+++ statistics.hs
@@ -21,7 +21,7 @@ countLetters (x:xs) = (sum [1|c<-(x:xs),c==x],x)
 Deluppgift 2
 -}
 maketree :: [(Integer, Char)] -> Htree
-maketree x = (createHtree)$(createWtree)$(sortWtree)$(makeWtree x)  
+maketree x = (createHtree)$(createOneWtree)$(sortWtree)$(makeWtree x)  
 
 createHtree :: Wtree -> Htree
 createHtree (L i1 w1)  = (Leaf w1)
@@ -38,46 +38,50 @@ sortWtree (x:xs) = sortBy (increasing') (x:xs)
 increasing' :: Wtree -> Wtree-> Ordering
 increasing' (L x _) (L y _)    = compare x y
 increasing' (B x _ _) (L y _)  = compare x y
-increasing' (L y _) (B x _ _)  = compare x y
+increasing' (L x _) (B y _ _)  = compare x y
 increasing' (B x _ _) (B y _ _)= compare x y
 
 
-createWtree :: [Wtree] -> Wtree  
-createWtree x
+createOneWtree :: [Wtree] -> Wtree  
+createOneWtree x
     | length x == 1 = head x
-    | otherwise = createWtree $(addWtree x)
+    | otherwise = createOneWtree $(addWtree x)
 
 
 addWtree :: [Wtree] -> [Wtree]
-addWtree ((L i1 c1):(L i2 c2):xs) = sortBy (increasing') 
-            (B (i1+i2) (L i1 c1) (L i2 c2):xs) 
-addWtree ((B i0 a b):(L i3 c3):xs)= sortBy (increasing') 
-            (B (i0+i3) (B i0 a b) (L i3 c3):xs)
-addWtree ((L i3 c3):(B i0 a b):xs)= sortBy (increasing') 
-            (B (i0+i3) (L i3 c3) (B i0 a b):xs)
+addWtree ((L i1 c1):(L i2 c2):xs)    = sortBy (increasing') 
+            ((B (i1+i2) (L i1 c1) (L i2 c2)):xs) 
+addWtree ((B i0 a b):(L i3 c3):xs)   = sortBy (increasing') 
+            ((B (i0+i3) (B i0 a b) (L i3 c3)):xs)
+addWtree ((L i3 c3):(B i0 a b):xs)   = sortBy (increasing') 
+            ((B (i0+i3) (L i3 c3) (B i0 a b)):xs)
 addWtree ((B i0 a b):(B i3 a2 b2):xs)= sortBy (increasing') 
-            (B (i0+i3) (B i0 a b) (B i3 a2 b2):xs)
+            ((B (i0+i3) (B i0 a b) (B i3 a2 b2)):xs)
 
 
 
-encode :: String -> Htree -> [Integer]
-encode [] _= (reverse [])
-encode (x:xs) htree =(traverseDF') htree x [] ++ encode xs htree
+encode :: String -> (Htree,[Integer])
+encode x = (htree,(test) x htree) 
+        where htree = (maketree) $(statistics) x
 
-traverseDF' :: Htree -> Char-> [Integer] -> [Integer]
-traverseDF' (Leaf c1) c output  = if c1==c then output else []
-traverseDF' (Branch l r) c output = (traverseDF' l c (1:output)) 
-                ++ (traverseDF' r c (0:output))
+test :: String -> Htree -> [Integer]
+test [] htree = (reverse) []
+test (x:xs) htree = ((traverseDF) htree x []) ++ test xs htree
+
+traverseDF :: Htree -> Char-> [Integer] -> [Integer]
+traverseDF (Leaf c1) c output  = if c1==c then output else []
+traverseDF (Branch l r) c output = (traverseDF l c (1:output)) 
+                ++ (traverseDF r c (0:output))
 {-
 let x = statistics "huffman"
 let y = makeWtree x
 let z = sortWtree y
-let w = createWtree z
+let w = createOneWtree z
 createHtree w
 
 2:
 let x = statistics "text"
 let y =maketree x
 encode "text" y
-traverseDF' y
+traverseDF y
 -}
