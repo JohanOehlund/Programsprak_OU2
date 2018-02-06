eee0cc9648bb03773d838f76ef5c8322004121ee
Johan Öhlund <c15jod@cs.umu.se>
2018-02-06 13:59:00 +0100

    alla uppgifter funkar nästan :p

--- HuffmanCoding.hs
+++ HuffmanCoding.hs
@@ -28,11 +28,11 @@ createHtree (L i1 w1)  = (Leaf w1)
 createHtree (B i1 w1 w2) = (Branch ((createHtree) w1) ((createHtree) w2))
 
 makeWtree :: [(Integer, Char)] -> [Wtree]
-makeWtree ((i,c):[]) = [L i c]
+makeWtree ((i,c):[]) = [L i c] 
 makeWtree ((i,c):xs) = L i c : makeWtree xs
 
 sortWtree :: [Wtree] -> [Wtree]
-sortWtree [] = []
+sortWtree [] =  []
 sortWtree (x:xs) = sortBy (increasing') (x:xs)
 
 increasing' :: Wtree -> Wtree-> Ordering
@@ -49,14 +49,10 @@ createOneWtree x
 
 
 addWtree :: [Wtree] -> [Wtree]
-addWtree ((L i1 c1):(L i2 c2):xs)    = sortBy (increasing') 
-            ((B (i1+i2) (L i1 c1) (L i2 c2)):xs) 
-addWtree ((B i0 a b):(L i3 c3):xs)   = sortBy (increasing') 
-            ((B (i0+i3) (B i0 a b) (L i3 c3)):xs)
-addWtree ((L i3 c3):(B i0 a b):xs)   = sortBy (increasing') 
-            ((B (i0+i3) (L i3 c3) (B i0 a b)):xs)
-addWtree ((B i0 a b):(B i3 a2 b2):xs)= sortBy (increasing') 
-            ((B (i0+i3) (B i0 a b) (B i3 a2 b2)):xs)
+addWtree ((L i1 c1):(L i2 c2):xs)    = sortBy (increasing') ((B (i1+i2) (L i1 c1) (L i2 c2)):xs) 
+addWtree ((B i0 a b):(L i3 c3):xs)   = sortBy (increasing') ((B (i0+i3) (B i0 a b) (L i3 c3)):xs)
+addWtree ((L i3 c3):(B i0 a b):xs)   = sortBy (increasing') ((B (i0+i3) (B i0 a b) (L i3 c3)):xs)
+addWtree ((B i0 a b):(B i3 a2 b2):xs)= sortBy (increasing') ((B (i0+i3) (B i0 a b) (B i3 a2 b2)):xs)
 
 
 
@@ -65,23 +61,45 @@ encode x = (htree,(test) x htree)
         where htree = (maketree) $(statistics) x
 
 test :: String -> Htree -> [Integer]
-test [] htree = (reverse) []
+test [] htree = []
 test (x:xs) htree = ((traverseDF) htree x []) ++ test xs htree
 
 traverseDF :: Htree -> Char-> [Integer] -> [Integer]
-traverseDF (Leaf c1) c output  = if c1==c then output else []
+traverseDF (Leaf c1) c output  = if c1==c then reverse output else []
 traverseDF (Branch l r) c output = (traverseDF l c (1:output)) 
                 ++ (traverseDF r c (0:output))
+
+decode :: Htree -> [Integer] -> String
+decode _ [] = []
+decode htree x = traverseHtree htree htree x []
+
+traverseHtree :: Htree -> Htree ->[Integer]-> String -> String
+traverseHtree _ (Leaf c1) [] output = reverse (c1:output)
+traverseHtree htree (Leaf c1) (x:xs) output = traverseHtree htree htree (x:xs) (c1:output)
+traverseHtree htree (Branch l r) (x:xs) output = if x == 0 then  traverseHtree htree r xs output else traverseHtree htree l xs output
+--traverseHtree htree _ [] output = reverse output
 {-
-let x = statistics "huffman"
+
+    | (Branch l r) && i==0 = traverseHtree r xs output
+    | (Branch l r) && i==1 = traverseHtree l xs output
+    | (Leaf c1) = c1
+    | otherwise = "illegal state..."
+
+
+let x = statistics "text"
 let y = makeWtree x
 let z = sortWtree y
 let w = createOneWtree z
 createHtree w
 
 2:
-let x = statistics "text"
-let y =maketree x
-encode "text" y
+
+encode "aaaa"
 traverseDF y
+
+DECODE:
+let x = encode "huffman"
+let y = fst x
+let z = snd x 
+decode y z
 -}
